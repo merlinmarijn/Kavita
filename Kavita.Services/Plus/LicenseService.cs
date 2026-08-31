a6908e1fddf2b356f672100d7ef318ec1fc5d8a2
merlinmarijn <marijnzijnmail@kpnmail.nl>
2026-08-31 10:25:38 +0200

    Fix formatting in HasActiveLicense method

--- Kavita.Services/Plus/LicenseService.cs
+++ Kavita.Services/Plus/LicenseService.cs
@@ -133,7 +133,7 @@ public class LicenseService(
     /// <returns></returns>
     public async Task<bool> HasActiveLicense(bool forceCheck = false, CancellationToken ct = default)
     {
-        return true
+        return true;
         var provider = cachingProviderFactory.GetCachingProvider(EasyCacheProfiles.License);
         if (!forceCheck)
         {
